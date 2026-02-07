1dcab4b3bf6edf22baf9477a56c5c87322ccc577
I-Maged <magedhanafy@gmail.com>
2026-02-07 04:28:09 +0200

    starter project

---
+++ src/components/ToggleSounds.jsx
@@ -0,0 +1,12 @@
+const ToggleSounds = ({ allowSound, setAllowSound }) => {
+  return (
+    <button
+      className='btn-sound'
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? '🔈' : '🔇'}
+    </button>
+  )
+}
+
+export default ToggleSounds
